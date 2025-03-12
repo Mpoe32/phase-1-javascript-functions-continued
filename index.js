6990751a23bc86462145168f931a300b80bb70cd
mpoe philip <philip.mpoe@student.moringaschool.com>
2025-03-12 17:20:54 +0300

    function continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// 1. saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // 2. mondayWork
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // 3. wrapAdjective
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
